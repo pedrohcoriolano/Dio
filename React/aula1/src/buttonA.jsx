f4d205f7b5821a832dbedf06de294f799175a23c
Pedro Henrique Coriolano Oliveira Cruz <93086323+pedrohcoriolano@users.noreply.github.com>
2022-04-20 23:38:35 -0300

    curso react

---
+++ React/aula1/src/buttonA.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function ComponenteA() {
+
+    return (
+        <h1>Componente A</h1>
+    )
+}
+
+export default ComponenteA
---
+++ React/aula1/src/buttonB.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function ComponenteB() {
+
+    return (
+        <h1>Componente B</h1>
+    )
+}
+
+export default ComponenteB
